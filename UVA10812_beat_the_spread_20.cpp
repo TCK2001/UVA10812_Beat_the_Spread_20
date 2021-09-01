15112e4438de8ce6780a29db2836f2913c5291ab
승강 <87925027+TCK2001@users.noreply.github.com>
2021-09-01 23:09:23 +0800

    Update UVA10812_beat_the_spread_20.cpp

--- UVA10812_beat_the_spread_20.cpp
+++ UVA10812_beat_the_spread_20.cpp
@@ -17,16 +17,16 @@ using namespace std;
 int main()
 {
 	int n;
-	cin>>n;
+	cin>>n; //how many times want to test.
 	for(int i=0;i<n;i++)
 	{
 		int a1=0,a2=0;
 		cin>>a1>>a2;
-		if(a1>a2&&(a1+a2)%2==0&&(a1-a2)%2==0)
+		if(a1>a2&&(a1+a2)%2==0&&(a1-a2)%2==0) //a1 most have to bigger than a2 & the score can't have a negative value;
 		{
 			cout<<(a1+a2)/2<<" "<<(a1-a2)/2<<endl;
 		}
-		else
+		else 
 		{
 			cout<<"impossible"<<endl;
 		}
